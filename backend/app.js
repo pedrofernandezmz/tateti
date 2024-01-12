37ed46a6f5712c0961bf205be5b2c433ac7c991e
Pedro Fernández <pedrofernandez@MacBook-Air-de-Pedro.local>
2024-01-12 01:22:31 -0300

    pruebas24/7

--- backend/app.js
+++ backend/app.js
@@ -123,101 +123,6 @@
 // });
 
 
-
-
-// import express from 'express';
-// import mysql from 'mysql2/promise';
-// import cors from 'cors';
-
-// const app = express();
-// app.use(cors());
-// const port = 8080;
-
-// // Configura la conexión a la base de datos MySQL en Google Cloud SQL
-// const createUnixSocketPool = async config => {
-//   return mysql.createPool({
-//     user: 'root',
-//     password: 'Cat123',
-//     database: 'mydb',
-//     socketPath: '/cloudsql/tateti-404421:southamerica-east1:tateti-555',
-//     ...config,
-//   });
-// };
-
-// // Crea un pool de conexión
-// const pool = await createUnixSocketPool({});
-
-// // Ruta para registrar un resultado de juego
-// app.get('/registrar-resultado', async (req, res) => {
-//   const { ganador } = req.query;
-
-//   try {
-//     // Obtén una conexión del pool
-//     const connection = await pool.getConnection();
-
-//     if (ganador === 'X') {
-//       await connection.query('UPDATE contador SET ganador_X = ganador_X + 1');
-//       console.log('Contador de ganador X actualizado');
-//       res.status(200).json({ message: 'Contador de ganador X actualizado' });
-//     } else if (ganador === 'O') {
-//       await connection.query('UPDATE contador SET ganador_O = ganador_O + 1');
-//       console.log('Contador de ganador O actualizado');
-//       res.status(200).json({ message: 'Contador de ganador O actualizado' });
-//     } else {
-//       await connection.query('UPDATE contador SET empate = empate + 1');
-//       console.log('Contador de empate actualizado');
-//       res.status(200).json({ message: 'Contador de empate actualizado' });
-//     }
-
-//     // Libera la conexión de nuevo al pool
-//     connection.release();
-//   } catch (err) {
-//     console.error('Error al actualizar el contador:', err);
-//     res.status(500).json({ error: 'Error al actualizar el contador' });
-//   }
-// });
-
-// // Ruta para obtener los resultados actuales
-// app.get('/obtener-resultados', async (req, res) => {
-//   try {
-//     const [result] = await pool.query('SELECT ganador_X, ganador_O, empate FROM contador');
-//     const row = result[0];
-//     const resultados = {
-//       ganador_X: row.ganador_X,
-//       ganador_O: row.ganador_O,
-//       empate: row.empate,
-//     };
-//     console.log('Mostrar Resultados');
-//     res.status(200).json(resultados);
-//   } catch (err) {
-//     console.error('Error al obtener resultados:', err);
-//     res.status(500).json({ error: 'Error al obtener resultados' });
-//   }
-// });
-
-// // Ruta para reiniciar los resultados (poner todo a 0)
-// app.post('/reiniciar-resultados', async (req, res) => {
-//   try {
-//     // Obtén una conexión del pool
-//     const connection = await pool.getConnection();
-//     await connection.query('UPDATE contador SET ganador_X = 0, ganador_O = 0, empate = 0');
-//     console.log('Resultados reiniciados');
-//     // Libera la conexión de nuevo al pool
-//     connection.release();
-//     res.status(200).json({ message: 'Resultados reiniciados' });
-//   } catch (err) {
-//     console.error('Error al reiniciar los resultados:', err);
-//     res.status(500).json({ error: 'Error al reiniciar los resultados' });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Aplicación escuchando en el puerto ${port}`);
-// });
-
-// export default app;
-
-
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
@@ -233,7 +138,7 @@ res.json({ status: "Bark bark! Ready to roll!" });
 });
 
 app.get("/razas", async (req, res) => {
-  const query = 'SELECT * FROM contador';
+  const query = 'SELECT * FROM razas';
   pool.query(query, (err, results) => {
     if (err) {
       console.error('Error al obtener razas de perros:', err);
@@ -254,6 +159,24 @@ app.get("/contador", async (req, res) => {
       res.json(results);
     }
   });
+});
+
+// Ruta para obtener los resultados actuales
+app.get('/obtener-resultados', async (req, res) => {
+  try {
+    const [result] = await pool.query('SELECT ganador_X, ganador_O, empate FROM contador');
+    const row = result[0];
+    const resultados = {
+      ganador_X: row.ganador_X,
+      ganador_O: row.ganador_O,
+      empate: row.empate,
+    };
+    console.log('Mostrar Resultados');
+    res.status(200).json(resultados);
+  } catch (err) {
+    console.error('Error al obtener resultados:', err);
+    res.status(500).json({ error: 'Error al obtener resultados' });
+  }
 });
 
 const pool = mysql.createPool({
