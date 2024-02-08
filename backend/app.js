35e9dc08eb78bb1d3aa5709948d1f39d9b8c99d2
Pedro Fernández <pedrofernandez@MacBook-Air-de-Pedro.local>
2024-02-08 14:28:50 -0300

    final

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'local') {
 }
 
 app.get("/", async (req, res) => {
-res.json({ status: "Backend Tateti Funcionando! Version 3.5.1" });
+res.json({ status: "Backend Tateti Funcionando! Version 3.5.1Final" });
 });
 
 // Ruta para registrar un resultado de juego
